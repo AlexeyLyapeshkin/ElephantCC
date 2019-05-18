c5d4b7c19dd6b68940de5f15d1c2b878c6d88d31
AlexeyLyapeshkin <alexey.shilo@gmail.com>
2019-05-18 12:12:30 +0300

    Second commit

--- DrawingTool.py
+++ DrawingTool.py
@@ -7,7 +7,34 @@ class DrawingTool:
     def create_canvas(self, x, y):
         self.MainArray = np.zeros((y, x), dtype=str)
 
+    def convert(self, list_):
+        i = 0
+        while i < len(list_):
+            if list_[i].isdigit():
+                list_[i] = int(list_[i])
+            i += 1
+        return list_
+
+    def parse_line(self, list_of_lines):
+
+        list = []
+
+        for i in range(len(list_of_lines)):
+            line = list_of_lines[i]
+            line = line.split()
+
+            list.append(line)
+        return list
+
         # self.MainArray[2][3] = 'x' # y, x
+
+    def read_from_file(self):
+
+        file_input = open('input.txt', 'r')
+        list_of_lines = file_input.readlines()
+
+        for i in self.parse_line(list_of_lines):
+            yield i
 
     def write_in_file(self):
 
@@ -57,6 +84,10 @@ class DrawingTool:
                 for i in range(y1, y2):
                     self.MainArray[i][x1] = 'x'
 
+            elif y1 == y2 and x1 != x2:
+                for i in range(x1, x2):
+                    self.MainArray[y1][i] = 'x'
+
             self.write_in_file()
 
         if mode.lower() == 'r':
@@ -64,9 +95,11 @@ class DrawingTool:
             x1, y1, x2, y2 = list_of_args
 
             if x1 != x2 and y1 != y2:
+
                 for i in range(x1, x2 + 1):
                     self.MainArray[y1][i] = 'x'
                     self.MainArray[y2][i] = 'x'
+
                 for i in range(y1, y2 + 1):
                     self.MainArray[i][x1] = 'x'
                     self.MainArray[i][x2] = 'x'
@@ -74,4 +107,40 @@ class DrawingTool:
             self.write_in_file()
 
         if mode.lower() == 'b':
-            pass
+
+            x, y, color = list_of_args
+
+            if x != y:
+                self.floodfill(x, y, color)
+
+            self.write_in_file()
+
+    def floodfill(self, x, y, color):
+
+        # assume surface is a 2D image and surface[x][y] is the color at x, y.
+
+        worldWidth = len(self.MainArray[0])
+        worldHeight = len(self.MainArray)
+
+        self.theStack = [(x, y)]
+
+        while len(self.theStack) > 0:
+
+            x, y = self.theStack.pop()
+
+            if self.MainArray[y][x] != '':
+                continue
+
+            self.MainArray[y][x] = color
+
+            if x < worldWidth - 1:
+                self.theStack.append((x + 1, y))  # right
+
+            if x > 0:
+                self.theStack.append((x - 1, y))  # left
+
+            if y < worldHeight - 1:
+                self.theStack.append((x, y + 1))  # down
+
+            if y > 0:
+                self.theStack.append((x, y - 1))  # up
--- Main.py
+++ Main.py
@@ -1,19 +1,18 @@
 from DrawingTool import DrawingTool
 
+
 def main():
 
-    file = open('input.txt','w')
-    file.close()
-    file = open('output.txt','w')
+    file = open('output.txt', 'w')
     file.close()
 
     Canvas = DrawingTool()
-    Canvas.draw('c',[200,40])
-    Canvas.draw('l',[0,0,0,3])
-    Canvas.draw('r', [0, 0, 3, 3])
+
+    for i in Canvas.read_from_file():
+        mode, *params = i
+        Canvas.draw(mode, Canvas.convert(params))
+
 
 
 if __name__ == '__main__':
     main()
-
-
